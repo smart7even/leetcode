472678d7a83a76dd23bf190efd72080062394814
smart7even <smart7even@yandex.ru>
2022-08-21 01:18:48 +0300

    feat: add task 1

---
+++ lib/task_0001.dart
@@ -0,0 +1,23 @@
+// Solution for https://leetcode.com/problems/two-sum/submissions/
+
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    var checkedNumbers = <int, int>{};
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+
+      var complimentaryNumber = target - num;
+      var complimentaryNumberIndex = checkedNumbers[complimentaryNumber];
+
+      if (complimentaryNumberIndex != null) {
+        return [complimentaryNumberIndex, i];
+      } else {
+        checkedNumbers[num] = i;
+      }
+    }
+
+    throw ArgumentError(
+        'Expected list of nums that have two integers which sum is $target');
+  }
+}
