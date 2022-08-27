170a772d09daa2ca7f6bd2dcfff70d393548d3c4
smart7even <smart7even@yandex.ru>
2022-08-27 12:18:38 +0300

    fix: correct task 1 link

--- lib/task_0001.dart
+++ lib/task_0001.dart
@@ -1,4 +1,4 @@
-// Solution for https://leetcode.com/problems/two-sum/submissions/
+// Solution for https://leetcode.com/problems/two-sum/
 
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
